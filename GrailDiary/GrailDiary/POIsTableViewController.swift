54d3177192353902402038903d0fc14ffcac1409
cladams0203 <cladams0203@gmail.com>
2020-10-20 17:49:18 -0400

    finished table view

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Chris Adams on 10/20/20.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
